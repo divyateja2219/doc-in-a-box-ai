bd8b1ef60ad32a01b14e4016a28b179347fed8d3
divyateja2219 <derugadi@gitam.in>
2025-07-31 21:27:19 +0530

    Delete src/pages/MedicineTracker.jsx

--- src/pages/MedicineTracker.jsx
+++
@@ -1,8 +0,0 @@
-export default function MedicineTracker() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">💊 Medicine Tracker</h1>
-      <p>This page will help monitor medicine schedules and reminders.</p>
-    </div>
-  );
-}
