af03920508e77eec29116864bb2ff34d42dd8421
divyateja2219 <derugadi@gitam.in>
2025-08-03 16:19:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+// src/App.tsx
+import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+function App() {
+return (
+<>
+<Hero />
+<Testimonials />
+</>
 );
+}
 
 export default App;
