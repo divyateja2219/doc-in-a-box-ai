61dd2354471cae09b1c75542c53f2d0b68a42a2e
divyateja2219 <derugadi@gitam.in>
2025-08-03 16:33:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+// src/App.tsx
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 function App() {
 return (
@@ -8,6 +10,7 @@ return (
 <Hero />
 <Features />
 <Testimonials />
+<Footer />
 </>
 );
 }
