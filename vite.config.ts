c4e6cf083ad69f2c3b27b8ddb1eab8e60fad337a
divyateja2219 <derugadi@gitam.in>
2025-08-12 08:42:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/doc-in-a-box-ai/',  // 👈 exact repo name here
   plugins: [react()],
 })
